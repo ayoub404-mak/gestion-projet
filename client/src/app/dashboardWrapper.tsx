8b4d49309d9ba1d8f1c0080687baad9ea460d511
ayoub404-mak <ayoub151539@gmail.com>
2026-05-20 11:34:37 +0200

    navbar + sidebar .

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
 
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-        {/*sidbar */}
-        sidebar
-        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
-            {/*navbar  */}
-            navbar
+        <Sidebar/>
+        <main 
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+            <Navbar />
             {children}
         </main>
     </div>
